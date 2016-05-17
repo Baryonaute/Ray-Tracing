59f8ba2be3d8a0931cdee5a5a63dbb146d975af4
a.gallais <aymeri.gallais@gmail.com>
2016-05-18 00:38:41 +0200

    Pour toi Paul, pour lancer MPI avec 2000 processus

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "mpi.h"
 #include <iostream>
 #include <fstream>
@@ -94,6 +95,8 @@ void savebmp(const char *filename, int width, int height, int dpi, RGBType *data
 }
 
 int main(int argc, char ** argv) {
+	clock_t t1, t2;
+	t1 = clock();
 
 	//std::cout << "Rendering..." << endl;
 	//ajout pour MPI
@@ -106,24 +109,34 @@ int main(int argc, char ** argv) {
 	//// MISE EN PLACE DE L'EXPERIENCE ////
 	///////////////////////////////////////
 
-	Light light1 = Light(Vector(100, 200, 500));
-	Light light2 = Light(Vector(255, 0, 200));
+	
+	
+
+	// Lights
+
+	Light light1 = Light(Vector(1280, 495, 70));
+	Light light2 = Light(Vector(1280, 465, 70));
 	vector<Light> lights;
 	lights.push_back(light1);
 	lights.push_back(light2);
 
-	Sphere sphere1 = Sphere(Vector(320, 500, 0), 100, Vector(10, 255, 10));
-	Sphere sphere2 = Sphere(Vector(400, 240, 0), 100, Vector(10, 10, 255));
-	Sphere sphere3 = Sphere(Vector(320, 240, 0), 100, Vector(255, 10, 10));
+
+	// Spheres
+
+	Sphere blue = Sphere(Vector(1040, 480, 70), 25, Vector(20, 20, 255), 0.9);
+	Sphere red = Sphere(Vector(640, 480, 0), 150, Vector(255, 20, 20), 0.9);
+	Sphere green = Sphere(Vector(240, 480, 0), 200, Vector(20, 255, 20), 0.9);
 	vector<Sphere> spheres;
-	spheres.push_back(sphere1);
-	spheres.push_back(sphere2);
-	spheres.push_back(sphere3);
+	spheres.push_back(blue);
+	spheres.push_back(red);
+	//spheres.push_back(green);
 
-	Camera camera = Camera(); // Camï¿½ra par dï¿½faut dans un premier temps
+
+	// Superstructure
+
+	Camera camera = Camera(); // Caméra par défaut. Deux autres constructeurs permettent des variantes.
 	Scene scene = Scene(spheres, lights);
-	RayTracer rayTracer = RayTracer(camera, scene, 0, 0.7, 0.5, 5);
-
+	RayTracer rayTracer = RayTracer(camera, scene, 0.5, 0.6, 8, 200);
 	//cout << scene << endl;
 	//cout << camera << endl;
 	//cout << rayTracer << endl;
@@ -142,15 +155,18 @@ int main(int argc, char ** argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);//initialisation de la variable my_rank
 	MPI_Comm_size(MPI_COMM_WORLD, &p);//nombre des processus
-	if (my_rank != 0) {int* pixels = new int[3*((width*height)/(p-1))];
-		/*for (int x = 0; x < width/p; ++x){
-		for (int y = 0; y < heigth; ++y) {*/
-cout<< "Process"<<my_rank<<endl;
-		for(int pixel = 0; pixel < (width*height)/(p-1); ++pixel){			
-/*pixel = y * width + x;*/
-int x = pixel%width;
-int y= (pixel -x)/width;
-y = y + (my_rank-1)*(height/(p-1));
+	if (my_rank != 0) {
+		int* pixels = new int[3 * ((width*height) / (p - 1))];
+		//x va de 0 à width
+		//y va de 0 à height
+		std::cout << "Process" << my_rank <<"is running"<< endl;
+		for (int pixel = 0; pixel < (width*height) / (p - 1); ++pixel) {
+			/*pixel = y * width + x;*/
+			//chaque processus va s'occupper d'un certain nombre de pixels
+			int x = ((my_rank-1)*((width*height)/(p-1))+pixel)%width;
+			//cet ajout est nécessaire pour plus de liberté dans le choix du nombre de proc
+			int y = (pixel - x) / width;
+			y = y + (my_rank - 1)*(height / (p - 1));
 			Ray ray = Ray(camera.eye, Vector(x, y, 0) - camera.eye);
 			Vector point = Vector();
 			bool alreadyIntersected = false;
@@ -187,30 +203,31 @@ y = y + (my_rank-1)*(height/(p-1));
 				pixels[3 * pixel + 2] = 255;
 			}
 		}
-
-		MPI_Send(pixels, 3 * (width*height)/(p-1), MPI_INT, 0, tag, MPI_COMM_WORLD);
-cout<<"Process"<<my_rank<<"job complete"<< endl;
-MPI_Finalize();
+		MPI_Send(pixels, 3 * (width*height) / (p - 1), MPI_INT, 0, tag, MPI_COMM_WORLD);
+		cout << "Process" << my_rank << "job complete" << endl;
+		delete pixels;
+		MPI_Finalize();
 	}
-	if (my_rank == 0) {cout<< "Process 0 start receiving"<<endl;
-		int* pixels; pixels = new int[3*(width*height)/(p-1)]; RGBType* resultat = new RGBType[n];
+	if (my_rank == 0) {
+		cout << "Process 0 start receiving" << endl;
+		int* pixels; pixels = new int[3 * (width*height) / (p - 1)]; RGBType* resultat = new RGBType[n];
 		for (source = 1; source < p; source++) {
-			MPI_Recv(pixels, 3 * (width*height)/(p-1), MPI_INT, source, tag, MPI_COMM_WORLD, &status);
-			cout<<"receiving"<<source<<endl;
-		for (int i = 0; i < (width*height)/(p-1); ++i) {
-	resultat[(source-1)*n/(p-1) + i].r = pixels[3 * i];
-	resultat[(source-1)*n/(p-1) + i].g = pixels[3 * i + 1];
-	resultat[(source-1)*n/(p-1)  + i].b = pixels[3 * i + 2];
-							}
+			MPI_Recv(pixels, 3 * (width*height) / (p - 1), MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+			cout << "receiving" << source << endl;
+			for (int i = 0; i < (width*height) / (p - 1); ++i) {
+				resultat[(source - 1)*n / (p - 1) + i].r = pixels[3 * i];
+				resultat[(source - 1)*n / (p - 1) + i].g = pixels[3 * i + 1];
+				resultat[(source - 1)*n / (p - 1) + i].b = pixels[3 * i + 2];
+			}
 		}
-savebmp("image_MPI.bmp", width, height, dpi, resultat);
-cout << "Image rendered successfully." << endl;
+		savebmp("image_MPI.bmp", width, height, dpi, resultat);
+		cout << "Image rendered successfully." << endl;
+		delete pixels, resultat;
+		t2 = clock();
+		float diff = (float)t2 - (float)t1;
+		cout << diff << endl;
+		MPI_Finalize();
+		
 	}
-
-
-
-	
-
 	return 0;
-
 }
